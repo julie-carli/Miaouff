2467049894cfb78f2262f6e7bf2335d7e3a331d5
julie <julie@thomass-air.home>
2024-10-20 14:06:54 +0200

    header

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('base.html')
 
-app.run(debug=True)
+@app.route('/animals')
+def animals():
+    return "Page des animaux"
+
+@app.route('/products')
+def products():
+    return "Page des produits"
+
+@app.route('/blog')
+def blog():
+    return "Blog"
+
+@app.route('/faq')
+def faq():
+    return "FAQ"
+
+if __name__ == '__main__':
+    app.run(debug=True)
